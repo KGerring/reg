5c96ae68fc80d368df97e5ecee619b9ce8cde271
KGerring <kgerring@gmail.com>
2020-06-14 19:03:26 -0400

    u

--- reg/__init__.py
+++ reg/__init__.py
@@ -7,3 +7,14 @@ from .error import RegistrationError
 from .predicate import (Predicate, KeyIndex, ClassIndex,
                         match_key, match_instance, match_class, PredicateRegistry)
 from .cache import DictCachingKeyLookup, LruCachingKeyLookup
+
+def make_tree():
+    """ show a directory of the project; requires the tree library"""
+    import subprocess
+    
+    test = subprocess.run(["which", "tree"], stdout=-1, stderr=-1, text=True)
+    if test.returncode == 0 and bool(test.stdout):
+        tree = subprocess.getoutput(
+                f"/usr/local/bin/tree -n {os.path.dirname(__file__)}"
+        )
+        sys.stdout.write(tree)
